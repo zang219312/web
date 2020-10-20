2b416e820c1830018f601daeda8cfc3b0e2c2fd3
zhang219312 <2324674973@qq.com>
2020-10-20 11:00:55 +0800

    目录修改

--- my/xcx/tc-demo/pages/demo/slide/slide.js
+++ my/xcx/tc-demo/pages/demo/slide/slide.js
@@ -5,34 +5,32 @@ Page({
    * 页面的初始数据
    */
   data: {
-    optionList: ['所有', '选项1', '选项2'],
-    value: '绑定条码',
     hideFlag: true, //true-隐藏  false-显示
     animationData: {}, //
-    state:0, // 1 已绑定 0 未绑定
-    isdisabled:false, // 是否禁用
+    state: 0, // 1 已绑定 0 未绑定
+    isdisabled: false, // 是否禁用
     switchChecked: false,
-    identify:false,  //true 识别成功 false 识别失败
-    remind:false, // 确认绑定
-    title:'ABO血型',
-    msg:'您可以自助录入条码，减少等待时间',
-    icon:'../../../images/icon-start.png',
-    barcode:'',
-    showRevoIcon:false, //false 隐藏撤销  true 撤销显示  
-    moni:0
+    identify: false, //true 识别成功 false 识别失败
+    remind: false, // 确认绑定
+    title: 'ABO血型',
+    msg: '您可以自助录入条码，减少等待时间',
+    icon: '../../../images/icon-start.png',
+    barcode: '',
+    showRevoIcon: false, //false 隐藏  true 显示  
+    moni: 0
   },
-  switchChange(e){
-    console.log(e.detail.value);
+  switchChange(e) {
     this.setData({
-      state: e.detail.value?1:0
+      state: e.detail.value ? 1 : 0
     })
-    
+
   },
 
   //取消
   mCancel: function () {
     var that = this;
     that.hideModal();
+    that.resetAll();
   },
 
   // 显示遮罩层
@@ -93,139 +91,173 @@ Page({
    */
   onLoad: function (options) {
     this.actions = new Map([
-      [1,['您可以自助录入条码，减少等待时间','../../../images/icon-start.png']],
-      [2,['条码格式不对哦，检查一下','../../../images/icon-validate.png']],
-      [3,['识别成功','../../../images/icon-scan-succ.png']],
-      [4,['绑定后讲无法修改条码，请确认','../../../images/warn.png']],
-      [5,['绑定失败，请再试一次','../../../images/bind-failure.png']],
-      [6,['绑定成功','../../../images/bind-success.png']]
+      [1, ['您可以自助录入条码，减少等待时间', '../../../images/icon-start.png']],
+      [2, ['条码格式不对哦，检查一下', '../../../images/icon-validate.png']],
+      [3, ['识别成功', '../../../images/icon-scan-succ.png']],
+      [4, ['绑定后讲无法修改条码，请确认', '../../../images/warn.png']],
+      [5, ['绑定失败，请再试一次', '../../../images/bind-failure.png']],
+      [6, ['绑定成功', '../../../images/bind-success.png']]
     ])
     //  条形码识别成功 才出现底部按钮
     // 文本不能修改
-
-    
   },
   // 去除空格
-  trim (char) { return char.replace(/\s+/g, ''); },
-  // 输入完成
-  bindconfirm(e){
-    let val = this.trim(e.detail.value);
+  trim(char) {
+    return char.replace(/\s+/g, '');
+  },
+  validate(str){
+    let val = this.trim(str);
+    // 正整数，0
+    let reg = /^\d+$/;
     
-    // 验证10位数
-    if(val.length === 10){
-      let action = this.actions.get(3) || [];
-      this.setData({
-        msg:action[0],
-        icon:action[1],
-        identify:true,
-        isdisabled:true,
-        barcode:val,
-        showRevoIcon:false
-      })
-    }else{
-      // 没过验证
-      let action = this.actions.get(2);
-      this.setData({
-        msg:action[0],
-        icon:action[1]
-      })
-    }
+    if(!reg.test(val)) return false;
+    // 验证长度 10位
+    if(val.length===10) return true;
+    return false;
 
   },
-  // 撤回上一步
-  revocation(){
+  // 输入完成
+  bindconfirm(e) {
+    console.log(e.detail.value);
+    
+    if(e.detail.value.length == 0) return;
+    if (this.validate(e.detail.value)) {
+      this.formatSucc(e.detail.value);
+    }else{
+      this.formatErr();
+    }
+  },
+  // 通过
+  formatSucc(val){
     let action = this.actions.get(3);
     this.setData({
-      msg:action[0],
-      icon:action[1],
-      identify:true,
-      isdisabled:true,
-      remind:false,
-      showRevoIcon:false,
-      barcode:this.data.barcode || '',
+      msg: action[0],
+      icon: action[1],
+      identify: true,
+      isdisabled: true,
+      barcode: val,
+      showRevoIcon: false
+    })
+  },
+  // 格式没有通过验证
+  formatErr(){
+    let action = this.actions.get(2);
+    this.setData({
+      msg: action[0],
+      icon: action[1]
+    })
+  },
+  resetAll() {
+    this.setData({
+      isdisabled: false, // 是否禁用
+      switchChecked: false,
+      identify: false, //true 识别成功 false 识别失败
+      remind: false, // 确认绑定
+      title: 'ABO血型',
+      msg: '您可以自助录入条码，减少等待时间',
+      icon: '../../../images/icon-start.png',
+      barcode: '',
+      showRevoIcon: false,
+    })
+  },
+  // 撤回上一步
+  revocation() {
+    this.resetAll()
+    this.setData({
+      barcode: this.data.barcode || '',
     })
   },
 
-  queding(){
+  queding() {
     // 确定 第一次提醒
     // 页面左上角加一个返回  返回上一步即清空数据
     this.setData({
-      msg:this.actions.get(4)[0],
-      icon:this.actions.get(4)[1],
-      identify:false,
-      remind:true,
-      showRevoIcon:true
+      showRevoIcon: true,
+      msg: this.actions.get(4)[0],
+      icon: this.actions.get(4)[1],
+      identify: false,
+      remind: true,
     })
   },
-  binding(){
+  binding() {
     // 确认绑定 交互
-    this.aa(this.data.moni).then(res=>{
+    this.aa(this.data.moni).then(res => {
       console.log(res);
-      if(res){
+      if (res) {
         this.setData({
-          state:1,
-          msg:this.actions.get(1)[0],
-          icon:this.actions.get(1)[1],
-          isdisabled:false,
-          remind:false,
-          switchChecked:true,
-          showRevoIcon:false
+          state: 1,
+          msg: this.actions.get(1)[0],
+          icon: this.actions.get(1)[1],
+          isdisabled: false,
+          remind: false,
+          switchChecked: true,
+          showRevoIcon: false
         })
       }
-    }).catch(err=>{
+    }).catch(err => {
       console.log(err);
       this.setData({
-        msg:this.actions.get(5)[0],
-        icon:this.actions.get(5)[1],
-        remind:false,
-        isdisabled:false,
-        showRevoIcon:false
+        msg: this.actions.get(5)[0],
+        icon: this.actions.get(5)[1],
+        remind: false,
+        isdisabled: false,
+        showRevoIcon: false
       })
     })
   },
   // 模拟交互
-  aa(e){
-    return new Promise((resolve,reject)=>{
-      if(e==1){
-      resolve(1)
-      }else{
+  aa(e) {
+    return new Promise((resolve, reject) => {
+      if (e == 1) {
+        resolve(1)
+      } else {
         reject('绑定失败')
       }
     })
   },
 
 
-  scan(){
-    console.log('scan');
+  scan() {
+    let that = this;
     // 只允许相机扫码
     wx.scanCode({
-      onlyFromCamera: true,  
-      // scanType:['barCode'],
-      success:(res)=>{
-        console.log(res);
+      onlyFromCamera: true,
+      scanType:['barCode'],
+      success: (res) => {
+        wx.showLoading({
+          title: '识别中...',
+        })
+        that.scanTimer = setTimeout(()=>{
+          wx.hideLoading()
+        },200)
+        if(that.validate(res.result)){
+          that.formatSucc(res.result)
+        }else{
+          that.formatErr();
+        }
       },
-      complete(e){
-        console.log(e);
+      fail(e){
+        console.error(e);
       }
     })
   },
 
-  call(){
-    this.apiCall().then(res=>{
+  call() {
+    this.apiCall().then(res => {
       console.log(res);
-    }).catch(err=>{
+    }).catch(err => {
       console.log(err);
     })
   },
 
-  apiCall(){
-    return new Promise((resolve,reject)=>{
+  apiCall() {
+    return new Promise((resolve, reject) => {
       wx.makePhoneCall({
         phoneNumber: '13647129407',
-        success(res){
+        success(res) {
           resolve(res)
         },
-        fail(err){
+        fail(err) {
           reject(err)
         }
       })
@@ -243,14 +275,15 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    console.log(this);
+    clearTimeout(this.scanTimer)
   },
 
   /**
