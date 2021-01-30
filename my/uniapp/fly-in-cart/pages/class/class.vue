bfc34f6bca781d1202d5108c5f4d7ac8b32cb673
zhangding <2324674973@qq.com>
2021-01-30 16:07:44 +0800

    新增添加购物车动画

--- my/uniapp/fly-in-cart/pages/class/class.vue
+++ my/uniapp/fly-in-cart/pages/class/class.vue
@@ -1,14 +1,22 @@
 <template>
 	<view>
-		
+		<ySteps :infoList="infoList"></ySteps>
+		<steps :options="options"></steps>
 	</view>
 </template>
 
 <script>
+	import ySteps from "../../components/Y-Steps/Y-Steps.vue";
+	import steps from '../../components/wangxiaohua--steps/index.vue'
 	export default {
+		components:{ySteps,steps},
 		data() {
 			return {
-				
+				infoList:[{date:'2020-01',info:' 内容 '},{date:'2020-01',info:' 内容 '},{date:'2020-01',info:' 内容 '},{date:'2020-01',info:' 内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容 '}],
+				lineNum:12,
+				options:[
+					{ text1: "2020-10-10 12:00", text2: "工单产生 1" }, { text1: "2020-10-10 12:00", text2: "工单产生 2" },{ text1: "2020-10-10 12:00", text2: "工单产生 2" }
+				]
 			}
 		},
 		methods: {
